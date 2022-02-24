aaf3fb7ae3168513d8d68a86eb9a2ca8f6ac1fa7
Ivan S <ilitetop@yandex.ru>
2022-02-24 15:12:14 +0700

    Добавлен класс исключений для Риских чисел

---
+++ Lab2/RomanNumberException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class RomanNumberException : Exception
+    {
+        internal RomanNumberException(string message) : base(message) { }
+        //internal RomanNumberException(string message, Exception inner) : base(message, inner) { }
+    }
+}
